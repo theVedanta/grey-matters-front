233f94d6f168120c775ad88cc53d1a0b0fbb2d53
Vedanta Somnathe <vedusom142005@gmail.com>
2021-06-07 17:20:26 +0530

    coder of the new world

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,55 +1,69 @@
 import React, { useEffect, useState } from "react";
-import "./Faq.css"
-import {FaInstagram, FaEnvelope, FaFacebookSquare} from 'react-icons/fa';
+import "./Faq.css";
+import { FaInstagram, FaEnvelope, FaFacebookSquare } from "react-icons/fa";
 import { IconBase } from "react-icons";
 import { withRouter } from "react-router-dom";
 
 function Faq() {
+  useEffect(() => {
+    fetchFaqs();
+  }, []);
 
-    useEffect(() => {
-        fetchFaqs();
-      }, []);
-    
-      let [faqs, setFaqs] = useState([]);
-    
-      async function fetchFaqs() {
-        const data = await fetch("https://grey-matters.herokuapp.com/faq");
-        const faqs = await data.json();
-        setFaqs(faqs.faqs);
-    }
+  let [faqs, setFaqs] = useState([]);
 
-    let SocialStyles = {color: "white", fontSize: "1.5vw", margin: "0 0.3vw"}
+  async function fetchFaqs() {
+    const data = await fetch("https://grey-matters.herokuapp.com/faq");
+    const faqs = await data.json();
+    setFaqs(faqs.faqs);
+  }
 
-    return (
-        <div className="faq faq-container">
-            <div className="faq faq-wrap">
-                <h1 className="faq faq-header">Frequently Asked Questions</h1>
-                <div className="units">
-                    {faqs.map((faq) => {
-                        return (
-                        <div className="faq faq-div">
-                            <div className="faq faq-question">
-                                <div className="faq faq-iconWrap">
-                                    <img src={process.env.PUBLIC_URL + 'images/plus.png'} className="faq faq-icon" />
-                                </div>
-                                <p className="faq faq-q">{faq.question}</p>
-                            </div>
-                            <div className="faq faq-answer hide">{faq.answer}</div>
-                        </div>
-                        )
-                    })}
+  let SocialStyles = { color: "white" };
+
+  return (
+    <div className="faq faq-container">
+      <div className="faq faq-wrap">
+        <h1 className="faq faq-header">Frequently Asked Questions</h1>
+        <div className="units">
+          {faqs.map((faq) => {
+            return (
+              <div className="faq faq-div">
+                <div className="faq faq-question">
+                  <div className="faq faq-iconWrap">
+                    <img
+                      src={process.env.PUBLIC_URL + "images/plus.png"}
+                      className="faq faq-icon"
+                    />
+                  </div>
+                  <p className="faq faq-q">{faq.question}</p>
                 </div>
-                <p className="faq faq-text">Got more questions? <a  className="faq faq-email" href = "mailto: greymattersdps@gmail.com">Contact Us</a></p>
-                <div className="faq connect"><p>Connect with us on</p>
-                    <span className="faq socials">
-                        <a href="https://www.instagram.com/grey._.matters/" target="_blank"><FaInstagram style={SocialStyles} /></a>
-                        <a href="mailto: greymattersdps@gmail.com" target="_blank"><FaEnvelope style={SocialStyles} /></a>
-                        <a href="https://www.facebook.com/GreyMattersDPSVK" target="_blank"><FaFacebookSquare style={SocialStyles} /></a>
-                    </span>
-                </div>
-            </div>
+                <div className="faq faq-answer hide">{faq.answer}</div>
+              </div>
+            );
+          })}
         </div>
-    )
+        <p className="faq faq-text">
+          Got more questions?{" "}
+          <a className="faq faq-email" href="mailto: greymattersdps@gmail.com">
+            Contact Us
+          </a>
+        </p>
+        <div className="faq connect">
+          <p>Connect with us on</p>
+          <span className="faq socials">
+            <a href="https://www.instagram.com/grey._.matters/" target="_blank">
+              <FaInstagram style={SocialStyles} />
+            </a>
+            <a href="mailto: greymattersdps@gmail.com" target="_blank">
+              <FaEnvelope style={SocialStyles} />
+            </a>
+            <a href="https://www.facebook.com/GreyMattersDPSVK" target="_blank">
+              <FaFacebookSquare style={SocialStyles} />
+            </a>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Faq
+export default Faq;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,41 +1,63 @@
-import React from 'react'
-import './Footer.css'
-import {FaGithub, FaFacebookSquare, FaBehance, FaInstagram} from "react-icons/fa"
+import React from "react";
+import "./Footer.css";
+import {
+  FaGithub,
+  FaFacebookSquare,
+  FaBehance,
+  FaInstagram,
+} from "react-icons/fa";
 
 function Footer() {
-
-  const Iconstyle = { color: "white", fontSize: "1.75vw" }
-  const mediaIcons = { color: "white", fontSize: "5vw"} 
+  const Iconstyle = { color: "white" };
+  const mediaIcons = { color: "white", fontSize: "5vw" };
 
   return (
-      <div className="footer footer-container">
-        <div className="footer footer-wrap">
-            <div className="footer-text">
-              <p className="footer header">Powered by <span className="cw">Code Warriors</span></p>  
+    <div className="footer footer-container">
+      <div className="footer footer-wrap">
+        <div className="footer-text">
+          <p className="footer header">
+            Powered by <span className="cw">Code Warriors</span>
+          </p>
 
-              <div className="footer media socials-wrap">
-                <div className="footer media socials">
-                  <a href="https://www.facebook.com/codewarriors/"><FaFacebookSquare style={Iconstyle}/></a>
-                  <a href="https://www.instagram.com/codewarriors42/?hl=en"><FaInstagram style={Iconstyle}/></a>
-                  <a href="https://github.com/codewarriors42"><FaGithub style={Iconstyle}/></a>
-                  <a href="https://www.behance.net/codewarriors"><FaBehance style={Iconstyle}/></a>
-                </div>
-              </div>    
+          <div className="footer media socials-wrap">
+            <div className="footer media socials">
+              <a href="https://www.facebook.com/codewarriors/">
+                <FaFacebookSquare style={Iconstyle} />
+              </a>
+              <a href="https://www.instagram.com/codewarriors42/?hl=en">
+                <FaInstagram style={Iconstyle} />
+              </a>
+              <a href="https://github.com/codewarriors42">
+                <FaGithub style={Iconstyle} />
+              </a>
+              <a href="https://www.behance.net/codewarriors">
+                <FaBehance style={Iconstyle} />
+              </a>
+            </div>
+          </div>
 
-              <p className="footer para">Mahika, Vedanta- Codimg</p>
-              <p className="footer para">Armaan- Designing</p>    
-            
-            </div>    
-            <div className="footer big socials-wrap">
-              <div className="footer big socials">
-                <a href="https://www.facebook.com/codewarriors/"><FaFacebookSquare style={Iconstyle}/></a>
-                <a href="https://www.instagram.com/codewarriors42/?hl=en"><FaInstagram style={Iconstyle}/></a>
-                <a href="https://github.com/codewarriors42"><FaGithub style={Iconstyle}/></a>
-                <a href="https://www.behance.net/codewarriors"><FaBehance className="reactIcons" style={Iconstyle}/></a>
-              </div>
-            </div>  
+          <p className="footer para">Mahika, Vedanta- Codimg</p>
+          <p className="footer para">Armaan- Designing</p>
+        </div>
+        <div className="footer big socials-wrap">
+          <div className="footer big socials">
+            <a href="https://www.facebook.com/codewarriors/">
+              <FaFacebookSquare style={Iconstyle} />
+            </a>
+            <a href="https://www.instagram.com/codewarriors42/?hl=en">
+              <FaInstagram style={Iconstyle} />
+            </a>
+            <a href="https://github.com/codewarriors42">
+              <FaGithub style={Iconstyle} />
+            </a>
+            <a href="https://www.behance.net/codewarriors">
+              <FaBehance className="reactIcons" style={Iconstyle} />
+            </a>
+          </div>
         </div>
       </div>
-)}
+    </div>
+  );
+}
 
-export default Footer
+export default Footer;
