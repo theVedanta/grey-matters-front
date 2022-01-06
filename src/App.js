1646236563e753d6466eee648ae78d6a39988083
Vedanta Somnathe <vedanta1412@gmail.com>
2022-01-06 19:56:06 +0530

    redirect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
@@ -10,27 +9,38 @@ import Archive from "./components/Archive";
 import Announcements from "./components/Announcements";
 import Sidebar from "./components/Sidebar";
 import Err from "./components/Err";
+import Red from "./components/Red";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect,
+} from "react-router-dom";
 
 const App = () => {
-  return (
-    <Router>
-      <React.Fragment>
-        <NavBar />
-        <Sidebar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/archive" component={Archive} />
-          <Route path="/announcements" component={Announcements} />
-          <Route path="/members" component={Members} />
-          <Route path="/alumni" component={Alumni} />
-          <Route path="/*" exact component={Err} />
-        </Switch>
-        <Footer />
-      </React.Fragment>
-    </Router>
-  );
+    return (
+        <Router>
+            <React.Fragment>
+                <NavBar />
+                <Sidebar />
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/archive" exact component={Archive} />
+                    <Route
+                        path="/announcements"
+                        exact
+                        component={Announcements}
+                    />
+                    <Route path="/members" exact component={Members} />
+                    <Route path="/alumni" exact component={Alumni} />
+                    <Route path="/register" exact component={Red} />
+                    <Route path="/*" exact component={Err} />
+                </Switch>
+                <Footer />
+            </React.Fragment>
+        </Router>
+    );
 };
 
 export default App;
---
+++ src/components/Red.jsx
@@ -0,0 +1,11 @@
+import { useEffect } from "react";
+
+const Red = () => {
+    useEffect(() => {
+        window.location.href = "https://google.com/";
+    });
+
+    return "";
+};
+
+export default Red;
